4d3ff3e0588516b5a1081a482ac17d0c488e4329
Mccg <mccgtv@outlook.com>
2022-10-30 16:28:32 +0100

    Test 3 commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,5 +5,6 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("Test1");
         System.out.println("Test2");
+        System.out.println("Test3");
     }
 }
